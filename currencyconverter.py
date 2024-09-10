7d8407642164851911b749cdec65eee22aea28ba
DamianPark1 <148936875+DamianPark1@users.noreply.github.com>
2024-09-10 14:23:47 -0500

    Update currencyconverter.py

--- currencyconverter.py
+++ currencyconverter.py
@@ -1,11 +1,14 @@
+#Step 1: Import libraries
 import requests 
 
+#Define API Key, URL, and Currencies covered
 API_KEY = 'fca_live_id4xoE9bSKIfr4l7pQfe2BFhRs3VWdGpwdz4J44f'
 BASE_URL = f'https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}'
 
 CURRENCIES = ['USD', 'CAD', 'EUR', 'AUD', 'CNY']
 
-
+#Step 2: Run program
+#Define a function to check if the currency is in the list of valid currencies to be converted
 def convert_currency(base):
     currencies = ','.join(CURRENCIES)
     url = f'{BASE_URL}&base_currency={base}&currencies={currencies}'
@@ -16,6 +19,7 @@ def convert_currency(base):
     except:
         print('Invalid currency.')
         return None
+#Get input from user on what currency to convert
 while True:
     base = input('Enter the base currency (q for quit): ').upper()
 
@@ -27,6 +31,7 @@ while True:
         continue
 
     del data[base]
+#Prints converted currency value
     for ticker, value in data.items():
         print(f'{ticker}: {value}')
 
